27c46f997854d111cd9a58c8b7572a44a9d0839e
Davysongs <Davidgsongs@gmail.com>
2023-12-05 16:44:30 +0100

    dsd

--- EmmaSkills/skillapp/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class PasswordResetForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-
-class ConfirmReset(forms.Form):
-    password = forms.PasswordInput(attrs = {'class': 'form-control', 'placeholder': 'Password'})
-    Conf_password = forms.PasswordInput(attrs = {'class': 'form-control', 'placeholder': 'Password'})
